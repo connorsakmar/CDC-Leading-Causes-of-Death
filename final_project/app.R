1a3f37db77f853d48256666d2c41be880a0d2808
Connor Sakmar <sakmarc@college.harvard.edu>
2018-11-28 20:18:32 -0500

    Have created the code for the scatter plot and bar graph, will move onto the map next

---
+++ final_project/app.R
@@ -0,0 +1,169 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(shinythemes)
+library(plotly)
+library(dplyr)
+library(kableExtra)
+library(tidytext)
+library(knitr)
+library(janitor)
+library(lubridate)
+library(stringr)
+library(fs)
+library(formattable)
+library(ggplot2)
+library(gganimate)
+library(tidyverse)
+library(leaflet)
+install.packages(shinydashboard)
+
+leading_deaths <- read.csv("https://data.cdc.gov/api/views/bi63-dtpu/rows.csv?accessType=DOWNLOAD")
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(fluidPage(theme = shinytheme("cerulean")),
+                
+                # Application title
+                titlePanel("Leading Causes of Death in the United States"),
+                
+                
+                # Sidebar with a slider input for number of bins 
+                sidebarLayout(
+                  sidebarPanel(
+                    conditionalPanel(
+                      condition = "input$x == 'Bar Graph'", 
+                    selectInput(inputId = "x",
+                                label = "Year:",
+                                choices = unique(leading_deaths$Year),
+                                selected = "1999")),
+                    
+                  
+                    
+                    conditionalPanel(
+                      condition = "input$Cause.Name == 'Scatter Plot'",
+                    selectInput(inputId = "Cause.Name", 
+                                label = "Cause of Death:",
+                                choices = unique(leading_deaths$Cause.Name),
+                                selected = "All Causes"))),
+                  
+                  
+                  
+                  # Show a plot of the generated distribution
+                  mainPanel(
+                    
+                    
+                    tabsetPanel(type = "tabs",
+                                tabPanel("About", htmlOutput("about")),
+                                tabPanel("The Top Causes", htmlOutput("top_causes")),
+                                tabPanel("Bar Graph", plotlyOutput("myPlot")),
+                                tabPanel("Scatter Plot", plotlyOutput("myPlot2")),
+                                tabPanel("Resources", htmlOutput("resources")))
+                    
+                    
+                  )
+                )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  
+  
+  #Creating a scatter plot
+  output$myPlot <- renderPlotly({
+    
+    myPlot1 <- leading_deaths %>%
+      select(Year, Deaths, State, Cause.Name) %>%
+      filter(Year == input$x) %>%
+      filter(State == "United States", Cause.Name != "All causes") %>%
+      ggplot(aes(x = Cause.Name, y = Deaths, fill = Cause.Name)) + geom_bar(stat = "identity") +
+      xlab("Year") + ylab("Number of Deaths") + 
+      ggtitle("Leading Causes of Death in the United States 1999-2016") +
+      theme(axis.text.x=element_blank(), axis.ticks.x=element_blank())
+    
+    require(scales)
+    myPlot1 + scale_y_continuous(labels = comma)
+    
+    
+  })
+  
+  output$myPlot2 <- renderPlotly({
+    
+    
+    
+    
+    myPlot2 <- leading_deaths %>%
+      filter(State == "United States") %>%
+      filter(Cause.Name == input$Cause.Name) %>%
+      ggplot(aes(x = Year, y = Deaths)) + geom_point() + geom_smooth()
+    
+    require(scales)
+    myPlot2 + scale_y_continuous(labels = comma)
+    
+    
+    
+    
+    
+  })
+  
+  output$about <- renderUI({
+    
+    # Provide users with a summary of the application and instructions
+    # Provide users with information on the data source
+    
+    str1 <- paste("About")
+    str2 <- paste("Leading Causes of Death")
+    str3 <- paste("What to explore?") 
+    str4 <- paste("Graphs. Data")
+    str5 <- paste("Source")
+    str6 <- paste("CDC")
+    
+    HTML(paste(h3(str1), p(str2), h3(str3), p(str4), h3(str5), p(str6)))})
+  
+  output$resources <- renderUI({
+    
+    #Resources about the leading causes of death 
+    
+    
+    str1 <- paste("Fill")
+    str2 <- paste("Fill")
+    str3 <- paste("Fill") 
+    str4 <- paste("Fill")
+    str5 <- paste("Fill")
+    str6 <- paste("Fill.")
+    str7 <- paste("Fill")
+    str8 <- paste("Fill")
+    
+    HTML(paste(h3(str1), p(str2), h3(str3), p(str4), h3(str5), p(str6), h4(str7), p(str8)))})
+  
+  
+  output$top_causes <- renderUI({
+    
+    #Explaining the leading causes of death
+    
+    str1 <- paste("Fill")
+    str2 <- paste("Fill")
+    str3 <- paste("Fill") 
+    str4 <- paste("Fill")
+    str5 <- paste("Fill")
+    str6 <- paste("Fill.")
+    str7 <- paste("Fill")
+    str8 <- paste("Fill")
+    
+    HTML(paste(h3(str1), p(str2), h3(str3), p(str4), h3(str5), p(str6), h4(str7), p(str8)))})}
+  
+  
+
+
+
+
+
+# Run the application 
+shinyApp(ui = ui, server = server)
