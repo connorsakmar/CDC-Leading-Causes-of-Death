1014c328029110435f5338adaf4e2b7e440b7c00
Connor Sakmar <sakmarc@college.harvard.edu>
2018-12-04 15:09:27 -0500

    Have added comments and updated the about tab on the shiny app

--- final_project/app.R
+++ final_project/app.R
@@ -30,7 +30,13 @@ library(sf)
 
 leading_deaths <- read.csv("https://data.cdc.gov/api/views/bi63-dtpu/rows.csv?accessType=DOWNLOAD")
 
+leading_deaths <- leading_deaths
+
+
 us_states_leading_deaths_2 <- read_rds("us_states_leading_deaths.rds")
+
+
+#Most of the comments for this code are included in the markdown file. 
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(fluidPage(theme = shinytheme("cerulean")),
@@ -43,6 +49,7 @@ ui <- fluidPage(fluidPage(theme = shinytheme("cerulean")),
                 sidebarLayout(
                   sidebarPanel(
                    
+                    #Allows every year in the dataset to be part of a  pulldown tab. 
                     selectInput(inputId = "x",
                                 label = "Year:",
                                 choices = unique(leading_deaths$Year),
@@ -50,11 +57,14 @@ ui <- fluidPage(fluidPage(theme = shinytheme("cerulean")),
                     
                   
                     
-                    
+                    #This code allows every cause of death in the dataset to be part of a pulldown *except* for all.causes. 
+                    #As stated in the markdown file, the 2016 data for all causes is incomplete and there is such a high
+                    #death toll for all causes compared to the singular causes of death that the scaling for the graphs 
+                    #are not as balanced. 
                     selectInput(inputId = "Cause.Name", 
                                 label = "Cause of Death:",
                                 choices = c("Alzheimer's Disease" = "Alzheimer's disease", "Cancer" = "Cancer", "CLRD" = "CLRD", "Diabetes" = "Diabetes",
-                                            "Heart Disease" = "Heart Disease", "Influenza and Pneumonia" = "Influenza and pneumonia", "Kidney Diseae" = "Kidney Disease",
+                                            "Heart Disease" = "Heart disease", "Influenza and Pneumonia" = "Influenza and pneumonia", "Kidney Disease" = "Kidney disease",
                                             "Stroke" = "Stroke", "Suicide" = "Suicide", "Unintentional Injuries" = "Unintentional injuries"),
                                 selected = "Unintentional Injuries")),
                   
@@ -63,14 +73,14 @@ ui <- fluidPage(fluidPage(theme = shinytheme("cerulean")),
                   # Show a plot of the generated distribution
                   mainPanel(
                     
-                    
+                    #Created tabs for each plot in addition to informational tabs for people that have no idea what the app is.
+                    #Also made a tab that makes general conclusions about each plot.
                     tabsetPanel(type = "tabs",
                                 tabPanel("About", htmlOutput("about")),
-                                tabPanel("The Top Causes", htmlOutput("top_causes")),
                                 tabPanel("Map", plotOutput("myMap")),
                                 tabPanel("Bar Graph", plotlyOutput("myPlot")),
                                 tabPanel("Scatter Plot", plotlyOutput("myPlot2")),
-                                tabPanel("Resources", htmlOutput("resources")))
+                                tabPanel("General Conclusions", htmlOutput("conclusions")))
                     
                     
                   )
@@ -130,26 +140,36 @@ server <- function(input, output) {
       filter(Cause.Name == input$Cause.Name)
     
     ggplot(data = us_states_leading_deaths_2) +
-      geom_sf(aes(fill = Deaths))
+      #Added a better gradient so that the differences between states are more obvious. 
+      geom_sf(aes(fill = Deaths)) + scale_fill_gradient(low = "white", high = "#009E73")
     
     
   })
   
   output$about <- renderUI({
     
-    # Provide users with a summary of the application and instructions
+    # Provide users with a summary of the application and what it is meant to 
+    # Provide useres with a some details about each plot. 
     # Provide users with information on the data source
     
     str1 <- paste("About")
-    str2 <- paste("Leading Causes of Death in the United States from 1999-2016. ")
+    str2 <- paste("This shiny shows the leading causes of death in the United States from 1999-2016. The data includes the top 10 causes 
+                  of death over that time, which are: Alzheimer's Disease, Cancer, CLRD (Chronic Lower Respiratory Disease), Diabetes,
+                  Heart Disease, Influenza and Pneumonia, Kidney Disease, Stroke, Suicide, and Unintentional Injuries.")
     str3 <- paste("What to explore?") 
-    str4 <- paste("There is a map ")
+    str4 <- paste("There are three plots that you can look at: a map, a bar graph, and a scatter plot. The map, as one might guess, maps
+                  the causes of death across the United States. A user can select both the year and cause of death that they want to see
+                  mapped across all 50 states. The bar graph allows the user to compare all of the leading of causes of death
+                  in the United States for a given year. The user can select which year they would like to compare the total deaths in the United
+                  States for each cause of death. Finally, the scatter plot allows the user to see the trend in deaths for any given cause of death
+                  from 1999-2016.")
     str5 <- paste("Source")
-    str6 <- paste("CDC")
+    str6 <- paste("This dataset came from the Center for Disease Control which compiled data from the National Center for Health Statistics.
+                  Cause of death statistics are based on the underlying cause of death.")
     
-    HTML(paste(h3(str1), p(str2), h3(str3), p(str4), h3(str5), p(str6)))})
+    HTML(paste(h1(str1), p(str2), h1(str3), p(str4), h1(str5), p(str6)))})
   
-  output$resources <- renderUI({
+  output$conclusions <- renderUI({
     
     #Resources about the leading causes of death 
     
