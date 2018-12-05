49a8ca00f50ef7c086a69fc7058d36d9a79329fe
Connor Sakmar <sakmarc@college.harvard.edu>
2018-12-04 19:27:36 -0500

    Added more comments to the shiny and added general conclusions

--- final_project/app.R
+++ final_project/app.R
@@ -33,10 +33,10 @@ leading_deaths <- read.csv("https://data.cdc.gov/api/views/bi63-dtpu/rows.csv?ac
 leading_deaths <- leading_deaths
 
 
+#Importing the RDS file made in the markdown file. 
 us_states_leading_deaths_2 <- read_rds("us_states_leading_deaths.rds")
 
 
-#Most of the comments for this code are included in the markdown file. 
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(fluidPage(theme = shinytheme("cerulean")),
@@ -91,7 +91,7 @@ ui <- fluidPage(fluidPage(theme = shinytheme("cerulean")),
 server <- function(input, output) {
   
   
-  
+  #See markdown file for additional comments on this code.
 
   output$myPlot <- renderPlotly({
     
@@ -101,8 +101,11 @@ server <- function(input, output) {
       filter(State == "United States", Cause.Name != "All causes") %>%
       ggplot(aes(x = Cause.Name, y = Deaths, fill = Cause.Name)) + geom_bar(stat = "identity") +
       xlab("Year") + ylab("Number of Deaths") + 
-      ggtitle("Leading Causes of Death in the United States 1999-2016") +
-      theme(axis.text.x=element_blank(), axis.ticks.x=element_blank())
+      ggtitle("Comparison of Leading Causes of Death in the U.S. 1999-2016") +
+      theme(axis.text.x=element_blank(), axis.ticks.x=element_blank()) +
+      
+    #  Changed the legend title to "Cause of Death" rather than Cause.Name
+      guides(fill=guide_legend(title="Cause of Death"))
     
     require(scales)
     myPlot1 + scale_y_continuous(labels = comma)
@@ -113,12 +116,14 @@ server <- function(input, output) {
   output$myPlot2 <- renderPlotly({
     
     
-    
+  #See markdwon file additional comments on this code.
     
     myPlot2 <- leading_deaths %>%
       filter(State == "United States", Cause.Name != "All causes") %>%
       filter(Cause.Name == input$Cause.Name) %>%
-      ggplot(aes(x = Year, y = Deaths)) + geom_point() + geom_smooth()
+      
+      #Added a title to the scatter plot
+      ggplot(aes(x = Year, y = Deaths)) + geom_point() + geom_smooth() + ggtitle("Trends of Leading Causes of Death in the U.S. 1999-2016") 
     
     require(scales)
     myPlot2 + scale_y_continuous(labels = comma)
@@ -132,6 +137,8 @@ server <- function(input, output) {
   
   
   output$myMap <- renderPlot({
+    
+    #See markdown file for additional comments on this code. 
     
     myMap <- 
       
@@ -156,13 +163,13 @@ server <- function(input, output) {
     str2 <- paste("This shiny shows the leading causes of death in the United States from 1999-2016. The data includes the top 10 causes 
                   of death over that time, which are: Alzheimer's Disease, Cancer, CLRD (Chronic Lower Respiratory Disease), Diabetes,
                   Heart Disease, Influenza and Pneumonia, Kidney Disease, Stroke, Suicide, and Unintentional Injuries.")
-    str3 <- paste("What to explore?") 
+    str3 <- paste("What to Explore") 
     str4 <- paste("There are three plots that you can look at: a map, a bar graph, and a scatter plot. The map, as one might guess, maps
                   the causes of death across the United States. A user can select both the year and cause of death that they want to see
                   mapped across all 50 states. The bar graph allows the user to compare all of the leading of causes of death
                   in the United States for a given year. The user can select which year they would like to compare the total deaths in the United
-                  States for each cause of death. Finally, the scatter plot allows the user to see the trend in deaths for any given cause of death
-                  from 1999-2016.")
+                  States for each cause of death. Finally, the scatter plot allows the user to see the trend in deaths for any cause of death
+                  that they select from 1999-2016.")
     str5 <- paste("Source")
     str6 <- paste("This dataset came from the Center for Disease Control which compiled data from the National Center for Health Statistics.
                   Cause of death statistics are based on the underlying cause of death.")
@@ -171,39 +178,31 @@ server <- function(input, output) {
   
   output$conclusions <- renderUI({
     
-    #Resources about the leading causes of death 
+    #General conclusions made from looking at the three plots. 
     
     
-    str1 <- paste("Fill")
-    str2 <- paste("Fill")
-    str3 <- paste("Fill") 
-    str4 <- paste("Fill")
-    str5 <- paste("Fill")
-    str6 <- paste("Fill.")
-    str7 <- paste("Fill")
-    str8 <- paste("Fill")
+    str1 <- paste("Map")
+    str2 <- paste("There is obviously a high correlation with population size and the amount of deaths in a state, which explains why California and
+                  Texas are always the most darkly shaded. However, the map is really interesting because it shows differences between eastern states and
+                  those in the Midwest; states that have relatively similar populations. It appears the many states in the midwest tend to be on the lower
+                  end of the death spectrum for most causes than some of the smaller eastern states.")
+    str3 <- paste("Bar Graph") 
+    str4 <- paste("The most distinctive aspect of the box plot is that heart disease and cancer are by far the two highest causes of death in the United States
+                  in comparison to the other eight top causes of death.CLRD, stroke, and unintentional injuries have also consistenly been in the top five causes of death,
+                  yet each of these accounts for less than a third of the deaths caused by heart disease (the leading cause of death) each year.")
+    str5 <- paste("Scatter Plot")
+    str6 <- paste("The scatter plot shows some striking trends in the leading causes of death. Seven of the ten causes have generally trended upward since 1999. 
+                  The three causes that don't show this trend are stroke, heart disease, and influenza and pneumonia. These all trended downards until around 2010,
+                  when they began to slowly trend upwards again.")
+    str7 <- paste("Conclusion")
+    str8 <- paste("Hopefully you enjoyed looking at this data and that it has encouraged you to look more closely and investigate some of the trends or patterns
+                  that you found!")
     
-    HTML(paste(h3(str1), p(str2), h3(str3), p(str4), h3(str5), p(str6), h3(str7), p(str8)))})
-  
-  
-  output$top_causes <- renderUI({
-    
-    #Explaining the leading causes of death
-    
-    str1 <- paste("CLRD")
-    str2 <- paste("Chronic Lower Respiratory Disease")
-    str3 <- paste("Fill") 
-    str4 <- paste("Fill")
-    str5 <- paste("Fill")
-    str6 <- paste("Fill.")
-    str7 <- paste("Fill")
-    str8 <- paste("Fill")
-    
-    HTML(paste(h3(str1), p(str2), h3(str3), p(str4), h3(str5), p(str6), h3(str7), p(str8)))})}
+    HTML(paste(h1(str1), p(str2), h1(str3), p(str4), h1(str5), p(str6), h1(str7), p(str8)))})}
   
   
 
-
+  
 
 
 
